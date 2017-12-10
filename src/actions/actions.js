8c1679168260906c60f5f776a9717cea5b8f6ada
Roshni Kutty <roshni.ajkutty@gmail.com>
2017-12-10 18:42:34 -0500

    updated for heroku

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,7 +1,7 @@
 import $ from 'jquery';
 
 const API_KEY = `a8457610b68381085a3fff38d6a36337:6:74255139`;
-const API_URL = `http://api.nytimes.com/svc/news/v3/content/all/all/168.json?api-key=${API_KEY}`;
+const API_URL = `https://api.nytimes.com/svc/news/v3/content/all/all/168.json?api-key=${API_KEY}`;
 
 export const GET_LANDING_ARTICLES_SUCCESS = 'GET_LANDING_ARTICLES_SUCCESS';
 export const getLandingArticlesSuccess = (articles) => ({
